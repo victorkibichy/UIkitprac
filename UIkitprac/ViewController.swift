9b221e94305734e501522a806f9912730d221c03
victor <victorkibichy>
2024-06-07 09:40:03 +0300

    distributions of constraints to the stack view

--- UIkitprac/ViewController.swift
+++ UIkitprac/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttona(_ sender: Any) {
+    }
+    @IBAction func rdytfyghk(_ sender: Any) {
+    }
 }
 
